42f79e52e893570e30db87eb3ecb874b5c59effd
k-2tha-brimm <kev.brimm@gmail.com>
2019-06-03 16:50:32 -0700

    add content type to header - server still does not render css

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"github.com/bmizerany/pat"
-	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -29,12 +30,33 @@ func confirmation(w http.ResponseWriter, r *http.Request) {
 	render(w, "./confirmation.html", nil)
 }
 
-func render(w http.ResponseWriter, filename string, data interface{}) {
-	tmpl, err := template.ParseFiles(filename)
+func render(w http.ResponseWriter, filename string, datum interface{}) {
+	path := filename
+	log.Println(path)
+	data, err := ioutil.ReadFile(string(path))
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-	if err := tmpl.Execute(w, data); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+	if err == nil {
+        var contentType string
+
+        if strings.HasSuffix(path, ".css") {
+            contentType = "text/css"
+        } else if strings.HasSuffix(path, ".html") {
+            contentType = "text/html"
+        } else if strings.HasSuffix(path, ".js") {
+            contentType = "application/javascript"
+        } else if strings.HasSuffix(path, ".png") {
+            contentType = "image/png"
+        } else if strings.HasSuffix(path, ".svg") {
+            contentType = "image/svg+xml"
+        } else {
+            contentType = "text/css"
+        }
+
+		w.Header().Add("Content-Type", contentType)
+		w.Write(data)
 	}
 }
